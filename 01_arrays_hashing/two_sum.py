1d5dea9df97ed10164efdc14740284bd768c965c
Indrakumar D R <indrakumardr@Indrakumars-MacBook-Air.local>
2025-05-15 21:26:29 +0530

    Initial setup: repo, folders, arrays & hashing

---
+++ 01_arrays_hashing/two_sum.py
@@ -0,0 +1,10 @@
+# Two Sum - LeetCode #1
+# Pattern: HashMap lookup
+def twoSum(nums, target):
+    hashmap = {}
+    for i, num in enumerate(nums):
+        diff = target - num
+        if diff in hashmap:
+            return [hashmap[diff], i]
+        hashmap[num] = i
+    return []
---
+++ patterns.md
@@ -0,0 +1,9 @@
+# DSA Patterns Cheatsheet
+
+Master patterns that cover 95% of LeetCode.
+
+## 1. Arrays & Hashing
+- HashMap Frequency
+- HashSet Membership
+- Two Pointers with HashMap
+...
